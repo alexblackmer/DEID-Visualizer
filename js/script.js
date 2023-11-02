4bc3fe7b468b0dfaa3e4cbb0c61086572ee527a9
grace3906 <gracebookworm15@gmail.com>
2023-11-01 18:17:22 -0600

    Get snow accumulation graph up (has same shape as SWE)

--- js/script.js
+++ js/script.js
@@ -1,14 +1,14 @@
 // ******* DATA LOADING *******
 // We took care of that for you
-async function loadData () {
-  const DEIDData = await d3.csv('data/DEID.csv');
-  const CLNData = await d3.csv('data/CLN.csv');
+async function loadData() {
+    const DEIDData = await d3.csv('data/DEID.csv');
+    const CLNData = await d3.csv('data/CLN.csv');
 
-  const parseTime = d3.timeParse("%m/%d/%Y %H:%M");
-  for (let obj of DEIDData) {
-    obj.Time = parseTime(obj.Time);
-  }
-  return { DEIDData, CLNData };
+    const parseTime = d3.timeParse("%m/%d/%Y %H:%M");
+    for (let obj of DEIDData) {
+        obj.Time = parseTime(obj.Time);
+    }
+    return {DEIDData, CLNData};
 }
 
 
@@ -16,27 +16,29 @@ async function loadData () {
 // This should be all you need, but feel free to add to this if you need to 
 // communicate across the visualizations
 const globalApplicationState = {
-  DEIDData: null,
-  CLNData: null,
-  densityChart: null,
-  SweChart: null,
+    DEIDData: null,
+    CLNData: null,
+    densityChart: null,
+    sweChart: null,
+    accChart: null,
 };
 
 
 //******* APPLICATION MOUNTING *******
 loadData().then((loadedData) => {
-  console.log('Here is the DEID data:', loadedData.DEIDData);
-  console.log('Here is the CLN data:', loadedData.CLNData);
+    console.log('Here is the DEID data:', loadedData.DEIDData);
+    console.log('Here is the CLN data:', loadedData.CLNData);
 
-  // Store the loaded data into the globalApplicationState
-  globalApplicationState.DEIDData = loadedData.DEIDData;
-  globalApplicationState.CLNData = loadedData.CLNData;
+    // Store the loaded data into the globalApplicationState
+    globalApplicationState.DEIDData = loadedData.DEIDData;
+    globalApplicationState.CLNData = loadedData.CLNData;
 
-  // Creates the view objects with the global state passed in 
-  const densityChart = new DensityChart(globalApplicationState);
-  const sweChart = new SweChart(globalApplicationState);
+    // Creates the view objects with the global state passed in
+    const densityChart = new DensityChart(globalApplicationState);
+    const sweChart = new SweChart(globalApplicationState);
+    const accChart = new AccChart(globalApplicationState);
 
-  globalApplicationState.densityChart = densityChart;
-  globalApplicationState.SWEChart = sweChart;
-
+    globalApplicationState.densityChart = densityChart;
+    globalApplicationState.sweChart = sweChart;
+    globalApplicationState.accChart = accChart;
 });
