e82a47f61b1118d6b2a102fd4379cd49699c4e5c
alexb <alexblackmer98@gmail.com>
2023-11-03 14:48:59 -0600

    Created density-height-chart.js, added rates to swe-chart.js and acc-chart.js, and cleaned up graph sizes

--- js/script.js
+++ js/script.js
@@ -21,6 +21,7 @@ const globalApplicationState = {
     densityChart: null,
     sweChart: null,
     accChart: null,
+    densityHeightChart: null
 };
 
 
@@ -37,8 +38,12 @@ loadData().then((loadedData) => {
     const densityChart = new DensityChart(globalApplicationState);
     const sweChart = new SweChart(globalApplicationState);
     const accChart = new AccChart(globalApplicationState);
+    const densityHeightChart = new DensityHeightChart(globalApplicationState)
+    // const cursor = new Cursor()
 
     globalApplicationState.densityChart = densityChart;
     globalApplicationState.sweChart = sweChart;
     globalApplicationState.accChart = accChart;
+    globalApplicationState.densityHeightChart = densityHeightChart;
+
 });
